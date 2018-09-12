ca9b5288247ef3da8d292a93ef9f7b3a3ebd6cd4
Adam Stokes <battlemidget@users.noreply.github.com>
2018-09-12 10:57:54 -0400

    Include file in container, link magic db
    Signed-off-by: Adam Stokes <battlemidget@users.noreply.github.com>

--- snap/docker-build.sh
+++ snap/docker-build.sh
@@ -6,5 +6,8 @@ docker run --rm -v "$PWD":/root/snap -w /root/snap \
        -e KUBE_VERSION=$(curl -L https://dl.k8s.io/release/stable.txt) \
        -v /usr/bin/curl:/usr/bin/curl \
        -v /usr/bin/make:/usr/bin/make \
+       -v /usr/share/misc/magic.mgc:/usr/share/misc/magic.mgc \
+       -v /usr/bin/file:/usr/bin/file \
+       -v /usr/lib/x86_64-linux-gnu/libmagic.so.1:/usr/lib/x86_64-linux-gnu/libmagic.so.1 \
        snapcore/snapcraft:stable \
        ./build-scripts/build "$@"
