04255d0b8f1947cecdcce4829279eec3151d1df8
Adam Stokes <battlemidget@users.noreply.github.com>
2018-09-12 10:41:00 -0400

    Fix docker builds
    This updates to a recent stable container for building snaps via docker.
    
    Signed-off-by: Adam Stokes <battlemidget@users.noreply.github.com>

--- snap/docker-build.sh
+++ snap/docker-build.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
-docker run --rm -v $PWD:/root/snap -w /root/snap -e SNAPCRAFT_SETUP_CORE=1\
-    snapcraft/xenial-amd64 make "$@"
+docker run --rm -v "$PWD":/root/snap -w /root/snap \
+       -e SNAPCRAFT_SETUP_CORE=1 \
+       -e KUBE_ARCH=amd64 \
+       -e KUBE_VERSION=$(curl -L https://dl.k8s.io/release/stable.txt) \
+       -v /usr/bin/curl:/usr/bin/curl \
+       -v /usr/bin/make:/usr/bin/make \
+       snapcore/snapcraft:stable \
+       ./build-scripts/build "$@"
